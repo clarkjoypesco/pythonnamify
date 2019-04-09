ab5627f449b2da874fd69c5f5cff0cfd02895269
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-09 10:00:50 +0800

    initial commit

---
+++ namify.py
@@ -0,0 +1,30 @@
+# Define a procedure, namify, that takes as
+# input a string, and returns a string that
+# is an uppercase 'N' followed by the input string.
+# for example, when you enter
+
+# print namify('ationals')
+
+# the output should be the string 'Nationals'
+
+
+def namify(s):
+    return 'N' + s
+
+
+
+
+
+
+
+
+# Remove the hash, #, from infront of print to test your code.
+
+print namify('orwegians')
+#>>> Norweigians
+
+print namify('orth')
+#>>> North
+
+print namify('ull')
+#>>> Null
